e22dc00183c647e90b877ef849c9d3e038531f29
JI-Maina <irungujmaina@gmail.com>
2023-08-08 22:48:31 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """Defines basic_auth authentication class
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+import binascii
 
 
 class BasicAuth(Auth):
@@ -9,15 +11,29 @@ class BasicAuth(Auth):
     """
 
     def extract_base64_authorization_header(
-            self,
-            authorization_header: str) -> str:
+            self, authorization_header: str) -> str:
         """returns the Base64 part of the Authorization header for a
         Basic Authentication"""
 
-        if authorization_header is None:
+        if authorization_header:
+            if isinstance(authorization_header, str):
+                if authorization_header.startswith("Basic "):
+                    return authorization_header[6:]
+                return None
             return None
-        if not isinstance(authorization_header, str):
+        return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """returns the decoded value of a Base64 string
+        base64_authorization_header"""
+
+        if base64_authorization_header:
+            if isinstance(base64_authorization_header, str):
+                try:
+                    x = b64decode(base64_authorization_header)
+                except (binascii.Error, UnicodeDecodeError):
+                    return None
+                return x.decode('utf-8')
             return None
-        if not authorization_header.startswith("Basic "):
-            return None
-        return authorization_header[6:]
+        return None
