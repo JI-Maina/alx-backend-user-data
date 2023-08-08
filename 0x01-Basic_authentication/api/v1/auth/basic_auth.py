e7a2eac67a5a5973775ee4b4544c2790719c2ca4
JI-Maina <irungujmaina@gmail.com>
2023-08-08 22:20:25 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,8 +14,10 @@ class BasicAuth(Auth):
         """returns the Base64 part of the Authorization header for a
         Basic Authentication"""
 
-        if authorization_header is None or
-        not isinstance(authorization_header, str) or
-        not authorization_header.startswith("Basic "):
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
