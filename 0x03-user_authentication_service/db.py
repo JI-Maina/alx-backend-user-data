754e60cb03ef77ee400cfc8aa643cc0ddf444257
JI-Maina <irungujmaina@gmail.com>
2023-08-17 14:26:44 +0000

    handled errors in add_user func

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -20,7 +20,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db")
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -37,12 +37,15 @@ class DB:
     def add_user(self, email: str, hashed_password: str):
         """Saves the user to the database and returns the saved User object
         """
-        if email and hashed_password:
+        try:
             user = User(email=email, hashed_password=hashed_password)
             session = self._session
             session.add(user)
             session.commit()
-            return user
+        except Exception:
+            session.rollback()
+            user = None
+        return user
 
     def find_user_by(self, **kwargs) -> User:
         """takes in arbitrary keyword arguments and returns the first row found
