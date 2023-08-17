717ea47b6dcaab5ae2bd86fc3cb0926abf5a0ead
JI-Maina <irungujmaina@gmail.com>
2023-08-17 21:37:24 +0000

    Logged in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,7 +51,8 @@ def login():
 
     session_id = AUTH.create_session(email)
 
-    response = jsonify({"email": f"{email}", "message": "logged in"})
+    msg = jsonify({"email": f"{email}", "message": "logged in"})
+    response = response.make_response(msg)
     response.set_cookie("session_id", session_id)
 
     return response
